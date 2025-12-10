6c0196674a4615623b3e8305ff247fee302f15dc
aboosam532002-source <aboosam532002@gmail.com>
2025-12-10 14:07:52 +0200

    Update answer.js

--- api/voice/answer.js
+++ api/voice/answer.js
@@ -1,1 +1,10 @@
+export default async function handler(req, res) {
+  const ncco = [
+    {
+      action: "talk",
+      text: "Hello, this is your verification code. Please enter the digits sent to your phone."
+    }
+  ];
+  res.status(200).json(ncco);
+}
 
